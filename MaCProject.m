448b8f6c38a1b809e6c022094a87f47690ede1df
Theo Lepoutte <theolepoutte@gmail.com>
2020-03-19 18:31:30 +0100

    Add Correct Time Scale multiple of T

--- MaCProject.m
+++ MaCProject.m
@@ -23,61 +23,72 @@ SymRate= 1/T;               % Symbol Rate
 %------------------------
 
 bn_tx = (randi(2,1,Nb)-1)';                 % bn = Binary sequence
-In_tx = mapping(bn_tx,Nbps,'qam');          % In = Symbols
+In_tx = mapping(bn_tx,Nbps,'qam');          % In = Symbols sequence at transmitter
 
 
-N = 5000; %must be even number
-i = 1;
-fmax = (1+RollOff)/(2*T) + 50000;
+N = 10000;                                  % N = Number of samples
+k = 10;                                     % k = Number of samples btw every symbol period in h(t)
+%fratio = 16;                               % fratio = Ratio between fzero and fmax  -> zero padding
+%fzero =(1+RollOff)/(2*T);                  % fzero = frequency at which the gain is equal to zero
+dt = T/k;                                   % time scale : 1 sample = dt [s]
+df = 1/(dt*N);                              % frequency scale : 1 sample = df [Hz]
 
-for f = 0: fmax/N :fmax
-    if (f<(1-RollOff)/(2*T))
+
+for i=1:N
+    if ((i-1)*df<(1-RollOff)/(2*T))
        H(i)=T;
-    elseif(f<(1+RollOff)/(2*T))
-       H(i)=T*(1+cos(pi*T*(f-(1-RollOff)/(2*T))/RollOff))/2;  
-       H(i)=T*(1+cos(pi*T*(f-(1-RollOff)/(2*T))/RollOff))/2; 
+    elseif((i-1)*df<(1+RollOff)/(2*T))
+       H(i)=T*(1+cos(pi*T*((i-1)*df-(1-RollOff)/(2*T))/RollOff))/2;  
     else
        H(i)=0;
-       H(i)=0;  
     end
     i = i+1;
 end
 
-h = ifft(H);
-t = 0:length(h)-1;
-figure;
-fr = 0: fmax/N :fmax;
 
-plot(fr,H)
+h = ifftshift(ifft(H,'symmetric'));
+tscale = -dt*N/2:dt:(N/2-1)*dt;
+fscale = 0: df : N*df-df;
+
+figure;
+plot(fscale,H)
 hold off;
 figure;
-plot(t,h,'r-');
+plot(tscale,h,'r-');
 hold off;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-In_rx(1:length(In_tx),1) = 0;
-
-for n=1:length(In_tx)
-    for m =1:1:length(In_tx)
-        if(n<m)
-            In_rx(n,1) = In_rx(n,1) + In_tx(m,1) * h(m-n+1);
-        elseif(n>m)
-            In_rx(n,1) = In_rx(n,1) + In_tx(m,1) * h(n-m+1);
-        else
-            In_rx(n,1) = In_rx(n,1) + In_tx(m,1) * h(1);
-        end
+es(1:Nb/Nbps)=0;
+for t = 1:Nb/Nbps
+    for n = 1:Nb/Nbps
+        if(t>n)
+             es(t)=es(t)+sum(In_tx(n)*h((t-n)*k));
+        end 
     end
-end 
-y = demapping(In_rx,6,'qam');
+end
+
+er=es;
+
+y(1:Nb/Nbps)=0;
+
+for t = 1:Nb/Nbps
+    for n = 1:Nb/Nbps
+        if(t>n)
+             y(t)=y(t)+sum(er(n)*h((t-n)*k));
+        end 
+    end
+end
+
+bn_rx = demapping(y',6,'qam');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 ber = 0; 
 for k=1:length(y)
-    if(y(k,1) ~= bn_tx(k,1))
+    if(bn_rx(k) ~= bn_tx(k))
         ber = ber+1;
     end
 end
-ber = ber/length(bn_tx) %%%%MARCHE PAS...?
+ber = ber/(Nb/Nbps)
 %-------------------------------------%
